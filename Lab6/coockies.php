c5ddef0f1c5a8aae2794af758e178eb4fc91c63a
ProgressBG-WWW-Courses <ProgressBG.WWW.Courses+gh@gmail.com>
2026-03-02 17:14:00 +0200

    TODO: Explain why not getting values in $_GET

---
+++ Lab6/coockies.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="script.js"></script>
+</head>
+<body>
+    <form action="" method="get">
+        <label>Select background color: <input name="color" id="color" type="color"></label>
+        <button id="submit" type="submit">Set Background Color</button>        
+    </form>
+</body>
+</html>
+
+<?php
+if (!empty($_GET)) {
+    foreach ($_GET as $key => $value) {
+        // htmlspecialchars prevents XSS when echoing user input
+        echo "key=" . htmlspecialchars($key) . ", value=" . htmlspecialchars($value) . "<br/>";
+    }
+} else {
+    echo "No data submitted yet.";
+}
+
+var_dump($_GET);
